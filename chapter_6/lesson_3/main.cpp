7135a2ba53cc60876afeb3c415455d0d078ac066
MagnusTrier <magnustrier@gmail.com>
2026-01-20 23:26:50 +0100

    Finished chapter 6

---
+++ chapter_6/lesson_3/main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+constexpr bool isEven(int num) {
+	return num % 2 == 0;
+}
+
+int main() {
+	std::cout << "Enter an integer: ";
+
+	int num;
+	std::cin >> num;
+
+
+	std::cout << num << " is " << (isEven(num) ? "even" : "odd") << '\n';
+	return 0;
+}
