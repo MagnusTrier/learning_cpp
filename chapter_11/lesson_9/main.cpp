0b71a1ecf8d4eb65bf6d7f4677ac109917bb8487
MagnusTrier <magnustrier@gmail.com>
2026-01-27 01:16:30 +0100

    Finished chapeter 12

---
+++ chapter_11/lesson_9/main.cpp
@@ -0,0 +1,21 @@
+
+template <int N> 
+constexpr int factorial() {
+	static_assert(N >= 0, "N must be bigger than or equal to 0.");
+
+	if constexpr (N <= 1) {
+		return 1;
+	} else {
+		return N * factorial<N-1>();
+	}
+}
+
+int main() {
+	static_assert(factorial<0>() == 1);
+	static_assert(factorial<3>() == 6);
+	static_assert(factorial<5>() == 120);
+
+	factorial<(-3)>();
+
+	return 0;
+}
---
+++ chapter_11/q2.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+
+template <typename T, typename U>
+auto sub(T x, U y) {
+	return x - y;
+}
+
+template <typename T>
+auto mult(T x, int y) {
+	return x * y;
+}
+
+template <typename T> T add(T x, T y) { return x + y; }
+
+int main() {
+	std::cout << "ADD:\n";
+	std::cout << add(2, 3) << '\n';
+	std::cout << add(1.2, 3.4) << '\n';
+	std::cout << "MULT:\n";
+	std::cout << mult(2, 3) << '\n';
+	std::cout << mult(1.2, 3) << '\n';
+	std::cout << "SUB:\n";
+	std::cout << sub(3, 2) << '\n';
+	std::cout << sub(3.5, 2) << '\n';
+	std::cout << sub(4, 1.5) << '\n';
+	return 0;
+}
